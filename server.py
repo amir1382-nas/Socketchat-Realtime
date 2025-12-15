71695a387b4e95010307e0eb913ee57786a7bb18
amir1382-nas <amir.nas1382@gmail.com>
2025-12-15 15:23:40 +0330

    update code server.py

--- server.py
+++ server.py
@@ -1,2 +1,24 @@
 #Professinal Socket Chat Server (single client)
 #real-time, threaded, whit proper error handling and message framing.
+
+import socket
+import threading
+from pyexpat.errors import messages
+
+
+def handle_receive(conn: socket.socket, stop_event: threading.Event):
+    """thread dedicated to receiving messages from client."""
+    try:
+        while not stop-stop_event.is_set():
+            data = conn.recv(BUFFER_SIZE)
+            stop_event.set()
+            break
+
+        messages = decode_message(data)
+        print(f"[CLIENT] {messages}")
+
+    except Exception as e:
+        print(f"[SERVR] Receive Error: {e}")
+        stop_event.set()
+
+
