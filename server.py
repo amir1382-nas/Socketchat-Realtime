6a88b65988a1ca673487a53ff16aedbfe4ab6da0
amir1382-nas <amir.nas1382@gmail.com>
2025-12-17 00:52:05 +0330

    update code start server def

--- server.py
+++ server.py
@@ -30,4 +30,36 @@ def start_server():
     server_socket.bind((HOST, PORT))
     server_socket.listen(1)
 
-    print
+    print(f"[SERVER] listening on {HOST}:{PORT} ...")
+    conn, addr = server_socket.accept()
+    print(f"[SERVER] connection accepted from {addr}")
+
+    stop_event = threading.Event()
+    #start receiving threa
+    receiver = threading.Thread(
+        target=handle_receive,
+        args=(conn, stop_event),
+        daemon=True
+    )
+    receiver.start()
+
+    try:
+        while not stop_event.is_set():
+            user_input = input()
+
+            if user_input.lower() in ("/quit", "/exit"):
+                print("[SERVER] shutting down...")
+                stop_event.set()
+                break
+
+            conn.sendall(encod_message(user_input))
+
+    finally:
+        stop_event.set()
+        conn.close()
+        server_socket.close()
+        print("[SERVER] closed all sockets. Byyyyye.")
+
+
+if __name__=="__main__":
+    start_server()
